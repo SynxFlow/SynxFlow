02af6a2fce114e4b06e9dbbaaad6937f451524a8
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-29 21:24:57 +0100

    Update __init__.py

--- pypims/IO/__init__.py
+++ pypims/IO/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Created on Wed Apr  1 14:56:15 2020
+
 """
 init
 To do:
     initialize a package
-Created on Wed Apr  1 14:56:15 2020
 
-@author: Xiaodong Ming
 """
 import numpy as np
 from .demo_functions import demo_input
